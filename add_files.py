8e30c9121f0110d1fe63e0ff98fbf71900014731
akashvshroff <akushroff@gmail.com>
2020-04-23 12:54:11 +0530

    Created a watchdog system for folder 'Safe'

---
+++ add_files.py
@@ -0,0 +1,51 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+import os
+import time
+import send2trash
+import sqlite3
+
+
+
+def on_any_event(event):
+    conn1 = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Personal Projects\Safe_sql\safe.sqlite')
+    cur = conn1.cursor()
+    for filename in os.listdir(path):
+        abs = os.path.join(path, filename)
+        name,ext = os.path.splitext(filename)
+        with open(abs, 'rb') as fhand:
+            binary_data = fhand.read()
+        file = binary_data
+        cur.execute('INSERT OR REPLACE INTO Files (name, ext, file) VALUES (?, ?, ?)',(name, ext, file,))
+        send2trash.send2trash(abs)
+    conn1.commit()
+
+
+conn = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Personal Projects\Safe_sql\safe.sqlite')
+
+cur1 = conn.cursor()
+cur1.executescript('''Create table if not exists Files (id INTEGER NOT NULL
+PRIMARY KEY AUTOINCREMENT UNIQUE, name TEXT, ext TEXT, file BLOB NOT NULL)''')
+cur1.close()
+
+patterns = "*"
+ignore_patterns = ""
+case_sensitive = False
+ignore_directories = False
+my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+
+my_event_handler.on_any_event = on_any_event
+
+path = r"C:\Users\akush\Desktop\Safe"
+go_recursively = True
+my_observer = Observer()
+my_observer.schedule(my_event_handler, path, recursive = go_recursively)
+
+my_observer.start()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    my_observer.stop()
+    my_observer.join()
