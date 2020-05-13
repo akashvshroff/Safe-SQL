521757c0813f72a941b0571d54c22a922e22987d
Akash Vikram Shroff <63399889+akashvshroff@users.noreply.github.com>
2020-05-13 23:38:11 +0530

    Fixed some bugs

--- add_files.py
+++ add_files.py
@@ -6,41 +6,48 @@ import time
 import send2trash
 import sqlite3
 
+
 def on_any_event(event):
-    conn1 = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Personal Projects\Safe_sql\safe.sqlite')
+    conn1 = sqlite3.connect(
+        r'C:\Users\akush\Desktop\Programming\Projects\Safe_Sql\safe.sqlite')
     cur = conn1.cursor()
     for filename in os.listdir(path):
         filename.strip()
         abs = os.path.join(path, filename)
-        name,ext = os.path.splitext(filename)
+        name, ext = os.path.splitext(filename)
         name = name.strip()
         ext = ext.strip()
         with open(abs, 'rb') as fhand:
             binary_data = fhand.read()
         file = binary_data
-        cur.execute('INSERT OR REPLACE INTO Files (name, ext, file) VALUES (?, ?, ?)',(name, ext, file,))
+        cur.execute('INSERT OR REPLACE INTO Files (name, ext, file) VALUES (?, ?, ?)',
+                    (name, ext, file,))
         send2trash.send2trash(abs)
     conn1.commit()
 
-if __name__=='__main__':
-    conn = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Personal Projects\Safe_sql\safe.sqlite') 
+
+if __name__ == '__main__':
+    conn = sqlite3.connect(
+        r'C:\Users\akush\Desktop\Programming\Projects\Safe_Sql\safe.sqlite')
 
     cur1 = conn.cursor()
-    cur1.executescript('''Create table if not exists Files (name TEXT, ext TEXT, file BLOB NOT NULL UNIQUE)''')
+    cur1.executescript(
+        '''Create table if not exists Files (name TEXT, ext TEXT, file BLOB NOT NULL UNIQUE)''')
     cur1.close()
 
     patterns = "*"
     ignore_patterns = ""
     case_sensitive = False
     ignore_directories = False
-    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
+    my_event_handler = PatternMatchingEventHandler(
+        patterns, ignore_patterns, ignore_directories, case_sensitive)
 
     my_event_handler.on_any_event = on_any_event
 
-    path = r"C:\Users\akush\Desktop\Safe" #Folder to observe
+    path = r"C:\Users\akush\Desktop\Safe"  # Folder to observe
     go_recursively = True
-    my_observer = Observer() #create an object
-    my_observer.schedule(my_event_handler, path, recursive = go_recursively)
+    my_observer = Observer()  # create an object
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
 
     my_observer.start()
     try:
--- view_safe.py
+++ view_safe.py
@@ -2,9 +2,10 @@ import sqlite3
 import os
 import sys
 
+
 def main():
     print("Welcome to the safe! Please enter your password")
-    password = "Barrys2e5!" #Password
+    password = "Barrys2e5!"  # Password
     while True:
         p = input()
         if p == password:
@@ -34,6 +35,7 @@ def main():
             print("Please re-enter!")
             continue
 
+
 def access():
     print("-"*15)
     print("Files in the safe \n")
@@ -43,26 +45,27 @@ def access():
     i = 1
     for row in rows:
         f = row[0] + row[1]
-        print(str(i),'. ',f)
+        print(str(i), '. ', f)
         fdict[i] = f
-        i+= 1
+        i += 1
     print("-"*15)
     print("Enter the number of the file you want to access. Enter x to return to menu.")
     a = input()
     if a == 'x':
         return
     filename = fdict[int(a)]
-    n,e = os.path.splitext(filename)
-    cur.execute('SELECT file FROM Files WHERE name = ? and ext = ?',(n,e,))
+    n, e = os.path.splitext(filename)
+    cur.execute('SELECT file FROM Files WHERE name = ? and ext = ?', (n, e,))
     try:
         data = cur.fetchone()[0]
-        abs = os.path.join(r'c:\Users\Akush\Desktop',filename)
+        abs = os.path.join(r'c:\Users\Akush\Desktop', filename)
         with open(abs, 'wb') as file:
             file.write(data)
-        cur.execute('DELETE FROM Files WHERE name = ? and ext = ?',(n,e,))
+        cur.execute('DELETE FROM Files WHERE name = ? and ext = ?', (n, e,))
         print("The file is on your desktop, remember to re-add to the safe folder after quitting the view safe.")
     except:
         print("File does not exist")
+
 
 def delete():
     print("-"*15)
@@ -73,28 +76,31 @@ def delete():
     i = 1
     for row in rows:
         f = row[0] + row[1]
-        print(str(i),'. ',f)
+        print(str(i), '. ', f)
         fdict[i] = f
-        i+=1
+        i += 1
     print("-"*15)
     print("Enter the number of the file you want to delete. Enter x to return to menu.")
     a = input()
     if a == 'x':
         return
     filename = fdict[int(a)]
-    n,e = os.path.splitext(filename)
+    n, e = os.path.splitext(filename)
     try:
-        cur.execute('DELETE FROM Files WHERE name = ? and ext = ?',(n,e,))
+        cur.execute('DELETE FROM Files WHERE name = ? and ext = ?', (n, e,))
         print("File deleted.")
     except:
         print("File does not exist")
+
 
 def quit():
     cur.close()
     conn.commit()
     sys.exit()
 
+
 if __name__ == '__main__':
-    conn = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Projects\Safe_sql\safe.sqlite') #Filename for your safe
-    cur = conn.cursor() #create a cursor connection, use OOP to facilitate the connection in the main function
+    # Filename for your safe
+    conn = sqlite3.connect(r'C:\Users\akush\Desktop\Programming\Projects\Safe_Sql\safe.sqlite')
+    cur = conn.cursor()  # create a cursor connection, use OOP to facilitate the connection in the main function
     main()
